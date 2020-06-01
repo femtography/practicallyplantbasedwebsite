83fc77491f60b33045eec4fb906240b6694ed1c7
Jeffrey Soto <femtography@me.com>
2020-06-01 13:05:33 -0600

    updated download link on recipe page and added req file for env

--- generator/urls.py
+++ generator/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view"),
+    path('downloads/', views.DownloadPDF.as_view(), name='pdf_download'),
     path('recipes/<int:amount>', views.recipe_list),
     path('recipe_gen', views.index),
     path('', views.home),
--- generator/views.py
+++ generator/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Recipe
 import random
+import io
 from io import BytesIO
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.template.loader import get_template
 from django.views import View
 from xhtml2pdf import pisa
+from reportlab.pdfgen import canvas
 
 # Create your views here.
 def index(request):
@@ -28,7 +30,6 @@ def recipe_list(request, amount):
         seasoning_i = seasoning_i + [x.replace(',', '') for x in r.seasoning.split('\r\n') if x.replace(',', '') not in seasoning_i ]
 
     global pdf_ing
-
     pdf_ing = {'ing_li': main_i, 'oils_li': oils_i, 'seasons_li': seasoning_i}
 
     args = {"recipes": r_list, 'ing_li': main_i, 'oils_li': oils_i, 'seasons_li': seasoning_i, 'bg_pic': bg_i[0]}
@@ -36,18 +37,39 @@ def recipe_list(request, amount):
 
     return render(request, 'generator/recipe_page.html', args)
 
-def render_to_pdf(template_src, context_dict={}):
-	template = get_template(template_src)
-	html  = template.render(context_dict)
-	result = BytesIO()
-	pdf = pisa.pisaDocument(BytesIO(html.encode("cp1252")), result)
-	if not pdf.err:
-		return HttpResponse(result.getvalue(), content_type='application/pdf')
-	return None
+def pdf_gen(item_dict={}):
+    buffer = io.BytesIO()
+    pdfl = canvas.Canvas(buffer)
+
+    x = 50
+    y = 750
+    pdfl.drawString(x, y, "Your Grocery List:")
+    y = y - 30
+
+    for key in item_dict.keys():
+        if key == 'oils_li':
+            y = y - 20
+            pdfl.drawString(x, y, "This is assuming that you have the following Oils/Sauces:")
+            y = y - 30
+        elif key == 'seasons_li':
+            y = y - 20
+            pdfl.drawString(x, y, "This is assuming that you have the following Seasonings:")
+            y = y - 30
+
+        for value in item_dict[key]:
+            pdfl.drawString(x, y, value)
+            y = y - 20
+
+    pdfl.showPage()
+    pdfl.save()
+
+    buffer.seek(0)
+    return buffer
 
 class ViewPDF(View):
     def get(self, request, *args, **kwargs):
-        pdf = render_to_pdf('generator/pdf_template.html', pdf_ing)
-        response = HttpResponse(pdf, content_type='application/pdf')
+        return HttpResponse(pdf_gen(pdf_ing), content_type='application/pdf')
 
-        return response
+class DownloadPDF(View):
+    def get(self, request, *args, **kwargs):
+        return FileResponse(pdf_gen(pdf_ing), as_attachment=True, filename='MyRecipe(s).pdf')
